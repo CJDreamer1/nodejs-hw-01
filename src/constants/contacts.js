cbbbccc7139134b618bee34e548c447fbce94026
Serhii Vasiliev <sepovi4@gmail.com>
2024-07-04 15:40:27 +0200

    risingProject

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,7 @@
-export const PATH_DB =
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+
+export const PATH_DB = fs
+  .readFile(path.resolve('src/db/db.json'))
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
