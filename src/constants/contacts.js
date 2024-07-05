a139e491def4a260aacfaad2d8e3e43c2d90a4d3
Serhii Vasiliev <sepovi4@gmail.com>
2024-07-05 14:40:01 +0200

    update1

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,7 +1,3 @@
-import * as fs from 'node:fs/promises';
 import path from 'node:path';
 
-export const PATH_DB = fs
-  .readFile(path.resolve('src/db/db.json'))
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
+export const PATH_DB = path.resolve('src/db/db.json');
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,45 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    // Читання існуючих контактів з файлу
+    let data;
+    try {
+      data = await fs.readFile(PATH_DB, 'utf-8');
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        // Якщо файл не існує, ініціалізуємо порожній масив
+        data = '[]';
+      } else {
+        throw error;
+      }
+    }
 
+    // Парсинг JSON
+    let contacts;
+    try {
+      contacts = JSON.parse(data);
+      if (!Array.isArray(contacts)) {
+        throw new Error('JSON data is not an array');
+      }
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
+      contacts = [];
+    }
+
+    // Генерація нових контактів
+    for (let i = 0; i < number; i++) {
+      contacts.push(createFakeContact());
+    }
+
+    // Запис оновленого масиву контактів назад у файл
+    await fs.appendFile(PATH_DB, JSON.stringify(contacts, null, 2));
+
+    console.log(`Added ${number} new contacts.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 generateContacts(5);
